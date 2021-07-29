a1e709bb8e0197bdefdf80e6bf34b7b79d031659
손효민 <sondo100@hanmail.net>
2021-07-30 00:55:36 +0900

    btClick

--- app/src/main/java/com/example/myapplication/OXquizIntroActivity.java
+++ app/src/main/java/com/example/myapplication/OXquizIntroActivity.java
@@ -2,13 +2,48 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OXquizIntroActivity extends AppCompatActivity {
 
+    Button oxStage1, oxStage2, oxStage3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        oxStage1 = findViewById(R.id.stage1BT);
+        oxStage2 = findViewById(R.id.stage2BT);
+        oxStage3 = findViewById(R.id.stage3BT);
+        View.OnClickListener oxClick;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oxquiz_intro);
+
+        oxClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()){
+                    case R.id.stage1BT:
+                        startActivity(new Intent(OXquizIntroActivity.this, OXquizStageActivity.class));
+                        break;
+                    case R.id.stage2BT:
+                        startActivity(new Intent(OXquizIntroActivity.this, OXquizStageActivity.class));
+                        break;
+                    case R.id.stage3BT:
+                        startActivity(new Intent(OXquizIntroActivity.this, OXquizStageActivity.class));
+                        break;
+                }
+            }
+        };
+
+        oxStage1.setOnClickListener(oxClick);
+        oxStage2.setOnClickListener(oxClick);
+        oxStage3.setOnClickListener(oxClick);
+
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/OXquizStageActivity.java
+++ app/src/main/java/com/example/myapplication/OXquizStageActivity.java
@@ -3,11 +3,36 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class OXquizStageActivity extends AppCompatActivity {
 
+    Button OXback,oBT,xBT;
+    TextView OXstage,OXstep,OXtime,OXscore, OXquiz, OXpopScore;
+    ImageView rightOimg,rightXimg,wrongOimg,wrongXimg;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        OXback = findViewById(R.id.backBT);
+        oBT = findViewById(R.id.oButton);
+        xBT = findViewById(R.id.xButton);
+
+        OXstage = findViewById(R.id.oxStage);
+        OXstep = findViewById(R.id.oxQuizStep);
+        OXtime = findViewById(R.id.oxTime);
+        OXscore = findViewById(R.id.oxScore);
+        OXquiz = findViewById(R.id.oxQuiz);
+        OXpopScore = findViewById(R.id.oxPopScore);
+
+        rightOimg = findViewById(R.id.rightOimg);
+        rightXimg = findViewById(R.id.rigntXimg);
+        wrongOimg = findViewById(R.id.wrongOimg);
+        wrongXimg = findViewById(R.id.wrongXimg);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oxquiz_stage);
     }
