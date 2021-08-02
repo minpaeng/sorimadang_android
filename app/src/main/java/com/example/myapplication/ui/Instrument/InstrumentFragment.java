d565c74b8e64204254ead7121d5f031ecdcdd2b7
손효민 <sondo100@hanmail.net>
2021-08-02 23:08:32 +0900

    instviewmodelDEL

--- app/src/main/java/com/example/myapplication/ui/Instrument/InstrumentFragment.java
+++ app/src/main/java/com/example/myapplication/ui/Instrument/InstrumentFragment.java
@@ -12,34 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentInstrumentBinding;
 
 public class InstrumentFragment extends Fragment {
 
-    private InstrumentViewModel instrumentViewModel;
-    private FragmentInstrumentBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        instrumentViewModel =
-                new ViewModelProvider(this).get(InstrumentViewModel.class);
 
-        binding = FragmentInstrumentBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textInstrument;
-        instrumentViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_instrument,container,false);
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
