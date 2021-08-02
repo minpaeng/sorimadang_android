4c316acd68bed9de15fc3b54a5f78a4ea97e94a0
손효민 <sondo100@hanmail.net>
2021-08-02 23:16:16 +0900

    songvmodelDEL

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -8,27 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.widget.ListViewCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
-import com.example.myapplication.IntroLastActivity;
-import com.example.myapplication.LoginActivity;
 import com.example.myapplication.OXquizIntroActivity;
-import com.example.myapplication.OXquizStageActivity;
 import com.example.myapplication.R;
-import com.example.myapplication.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static android.widget.Toast.LENGTH_SHORT;
 
 public class HomeFragment extends Fragment {
 
--- app/src/main/java/com/example/myapplication/ui/song/SongFragment.java
+++ app/src/main/java/com/example/myapplication/ui/song/SongFragment.java
@@ -4,44 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.myapplication.databinding.FragmentSongBinding;
+import com.example.myapplication.R;
 
 public class SongFragment extends Fragment {
 
-    private SongViewModel songViewModel;
-    private FragmentSongBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        songViewModel =
-                new ViewModelProvider(this).get(SongViewModel.class);
 
-        binding = FragmentSongBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View root = inflater.inflate(R.layout.fragment_song,container,false);
 
-        final TextView textView = binding.textSong;
-        songViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+          return root;
     }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
 
 }
