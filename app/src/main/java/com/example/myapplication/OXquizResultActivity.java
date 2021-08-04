759fc3d0ea34141a7f57f47337681e1b1249f5b1
Somang Kang <agoso3@naver.com>
2021-08-04 15:03:30 +0900

    made oxResult activity

---
+++ app/src/main/java/com/example/myapplication/OXquizResultActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OXquizResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oxquiz_result);
+        getSupportActionBar().hide();
+
+    }
+}
