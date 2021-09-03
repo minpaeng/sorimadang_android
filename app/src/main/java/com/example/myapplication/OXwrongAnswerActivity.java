7cd56fcf253bb1736c7a9f0df6f229f9fa36d83c
손효민 <sondo100@hanmail.net>
2021-09-03 20:16:18 +0900

    err

--- app/src/main/java/com/example/myapplication/OXwrongAnswerActivity.java
+++ app/src/main/java/com/example/myapplication/OXwrongAnswerActivity.java
@@ -16,15 +16,20 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OXwrongAnswerActivity extends AppCompatActivity {
 
-    String userIdToken=null;
+    static String userIdToken=null;
     TextView wrongAnswer;
+    String oxapiString=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +44,58 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
 
         try {
             //2)번 코드 호출
-            String apiString = new PostRestAPITask("http://sorimadang.shop/api/ox-game/wrong-questions/searchs").execute().get();
-            Log.v("f 성공 apiString", apiString);
-            JSONArray jarray=  new JSONArray(apiString);
+//            oxapiString = new PostRestAPITask("http://sorimadang.shop/api/ox-game/wrong-questions/searchs").execute().get();
+//            Log.v("성공 apiString", oxapiString);
+//            JSONArray jarray=  new JSONArray(oxapiString);
 
             //1)번 코드 호출
 //            JSONObject param = new JSONObject();
 //            // POST 방식으로 호출.(GET, POST, PUT, DELETE 다 가능 합니다.)
 //            HttpUtil.callApi(param, "POST");
+
+
+
+
+            URL obj = null;
+            String json = "";
+
+            String ID_TOKEN = URLEncoder.encode(userIdToken,"UTF-8");
+
+            Map<String, Object> params = new HashMap<String, Object>();
+            params.put("idToken", ID_TOKEN);
+            Log.v("API  성공","idtoken");
+
+            StringBuilder postData = new StringBuilder();
+            for(Map.Entry<String,Object> param : params.entrySet()) {
+                if(postData.length() != 0) postData.append('&');
+                postData.append(param.getKey());
+                postData.append('=');
+                postData.append(param.getValue());
+            }
+            byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+            Log.v("API  성공", String.valueOf(postDataBytes));
+
+            obj = new URL("http://sorimadang.shop/api/ox-game/wrong-questions/searchs");
+
+            HttpURLConnection con = (HttpURLConnection)obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            con.setDoOutput(true);
+            con.getOutputStream().write(postDataBytes);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), "UTF-8"));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while((inputLine = in.readLine()) != null){
+                response.append(inputLine);
+            }
+            in.close();
+
+            json = response.toString();
+            Log.v("API resopnse 성공",json);
+
+
         } catch (Exception e) {
             e.printStackTrace();
             Log.v("실패 apiString", "실패");
@@ -73,17 +122,14 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
 
             try {
                 //URL 설정
-                URL url = new URL("http://localhost:8080/test/api/action");
+                URL url = new URL("http://sorimadang.shop/api/ox-game/wrong-questions/searchs");
 
                 conn = (HttpURLConnection) url.openConnection();
 
                 // type의 경우 POST, GET, PUT, DELETE 가능
                 conn.setRequestMethod(type);
                 conn.setRequestProperty("Content-Type", "application/json");
-                conn.setRequestProperty("Transfer-Encoding", "chunked");
-                conn.setRequestProperty("Connection", "keep-alive");
                 conn.setDoOutput(true);
-
 
                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
                 // JSON 형식의 데이터 셋팅
@@ -91,13 +137,10 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
                 JSONArray jsonArray = new JSONArray();
 
 //                params.addProperty("key", 1);
-//                params.addProperty("age", 20);
-//                params.addProperty("userNm", "홍길동");
-//
 //                commands.add("userInfo", params);
-                // JSON 형식의 데이터 셋팅 끝
-
-                // 데이터를 STRING으로 변경
+//                // JSON 형식의 데이터 셋팅 끝
+//
+//                // 데이터를 STRING으로 변경
 //                Gson gson = new GsonBuilder().setPrettyPrinting().create();
 //                String jsonOutput = gson.toJson(commands);
 
@@ -156,8 +199,6 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
 
-                /*
-
                 conn.setDoInput(true); //input을 사용하도록 설정 (default : true)
                 conn.setDoOutput(true); //output을 사용하도록 설정 (default : false)
 
@@ -167,15 +208,14 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
                 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8")); //캐릭터셋 설정
 
                 writer.write(
-                    "key1=value1" +
-                    + "&key2=value2" +
-                    + "&key3=value3"
+                        "idToken="+userIdToken
                 ); //요청 파라미터를 입력
                 writer.flush();
                 writer.close();
                 os.close();
+                Log.v("성공 close: ","성공");
 
-                conn.connect();
+                //conn.connect();
 
                 BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); //캐릭터셋 설정
 
@@ -188,23 +228,21 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
                     sb.append(line);
                 }
 
-                * */
 
-
-
-                InputStream is = conn.getInputStream();
-
-                // Get the stream
-                StringBuilder builder = new StringBuilder();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
+//                InputStream is = conn.getInputStream();
+//
+//                // Get the stream
+//                StringBuilder builder = new StringBuilder();
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+//                String line;
+//                while ((line = reader.readLine()) != null) {
+//                    builder.append(line);
+//                }
 
                 // Set the result
-//                result = builder.toString();
-//                Log.v("성공doin: ",result);
+                result =sb.toString();//builder.toString();
+                Log.v("성공 doin: ",result);
+                Log.v("성공 doin2: ","끝");
             }
             catch (Exception e) {
                 // Error calling the rest api
