5a47e55fe8fdadbb2930500001f7aae151f91861
손효민 <sondo100@hanmail.net>
2021-09-03 17:16:55 +0900

    start

--- app/src/main/java/com/example/myapplication/OXwrongAnswerActivity.java
+++ app/src/main/java/com/example/myapplication/OXwrongAnswerActivity.java
@@ -156,6 +156,42 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
 
+                /*
+
+                conn.setDoInput(true); //input을 사용하도록 설정 (default : true)
+                conn.setDoOutput(true); //output을 사용하도록 설정 (default : false)
+
+                conn.setConnectTimeout(60); //타임아웃 시간 설정 (default : 무한대기)
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8")); //캐릭터셋 설정
+
+                writer.write(
+                    "key1=value1" +
+                    + "&key2=value2" +
+                    + "&key3=value3"
+                ); //요청 파라미터를 입력
+                writer.flush();
+                writer.close();
+                os.close();
+
+                conn.connect();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8")); //캐릭터셋 설정
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    if(sb.length() > 0) {
+                        sb.append("\n");
+                    }
+                    sb.append(line);
+                }
+
+                * */
+
+
+
                 InputStream is = conn.getInputStream();
 
                 // Get the stream
@@ -167,8 +203,8 @@ public class OXwrongAnswerActivity extends AppCompatActivity {
                 }
 
                 // Set the result
-                result = builder.toString();
-                Log.v("성공doin: ",result);
+//                result = builder.toString();
+//                Log.v("성공doin: ",result);
             }
             catch (Exception e) {
                 // Error calling the rest api
