7cdefc5d707cd19d0c6ab0596c998aaf6b7a2d9b
hmson18 <sondo100@hanmail.net>
2021-09-30 23:08:27 +0900

    intro

--- app/src/main/java/com/example/myapplication/GwanInstStudyIntro.java
+++ app/src/main/java/com/example/myapplication/GwanInstStudyIntro.java
@@ -7,14 +7,14 @@ import android.widget.ImageButton;
 
 public class GwanInstStudyIntro extends AppCompatActivity {
 
-    ImageButton imgbt1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gwan_inst_study_intro);
 
-        imgbt1 = findViewById(R.id.imageButton2);
+
 
     }
 }
